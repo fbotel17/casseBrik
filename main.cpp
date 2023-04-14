35f1a598689e5f3423006d99ed973c60ce99411b
fbotel17 <faustbtl17@gmail.com>
2023-04-14 11:43:45 +0200

    Ajoutez des fichiers projet.

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <SFML/Graphics.hpp>
+#include "Ball.h"
+
+double mapValue(double x, double a, double b, double c, double d) {
+	double y = (x - a) / (b - a) * (d - c) + c;
+	return y;
+}
+
+
+int main(int argc, char **argv)
+{
+	Ball ball(200, 250, 10, 600);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "ma magnifique fenetre");
+
+	sf::RectangleShape rectangle;
+	rectangle.setSize(sf::Vector2f(100, 50));
+	rectangle.setFillColor(sf::Color::Cyan);
+	rectangle.setOutlineColor(sf::Color::Magenta);
+	rectangle.setOutlineThickness(2);
+	rectangle.setPosition(100, 100);
+
+	sf::CircleShape circle;
+	circle.setRadius(10);
+	circle.setPosition(200, 170);
+	circle.setFillColor(sf::Color::Red);
+	circle.setOutlineColor(sf::Color::Yellow);
+	circle.setOutlineThickness(2);
+
+	sf::RectangleShape rdr2;
+	rdr2.setSize(sf::Vector2f(window.getSize().x, window.getSize().y));
+
+	sf::Clock clock;
+	sf::Vector3f xFactor(10, 20, 30);
+	float ellapsedTime = 0;
+
+
+	// on fait tourner le programme jusqu'à ce que la fenêtre soit fermée
+	while (window.isOpen())
+	{
+		ellapsedTime = clock.restart().asSeconds();
+
+		// on inspecte tous les évènements de la fenêtre qui ont été émis depuis la précédente itération
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// évènement "fermeture demandée" : on ferme la fenêtre
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		
+		ball.move(ellapsedTime);
+		ball.manageCollisionWith(window);
+
+		window.clear();
+
+		for (int i = 0; i < window.getSize().y; i++)
+		{
+			double mappedValue = mapValue(i, 0, window.getSize().y, 0, 255);
+			rdr2.setFillColor(sf::Color(mappedValue, 0, 0));
+			rdr2.setPosition(0, i);
+			window.draw(rdr2);
+
+			/*double mappedValue = mapValue(i, 0, window.getSize().y, 0, 255);
+			rdr2.setFillColor(sf::Color(mappedValue, mappedValue * xFactor.x * ellapsedTime, mappedValue * xFactor.y * ellapsedTime, mappedValue * xFactore.z * ellapsedTime));
+			rdr2.setPosition(0, i);
+			window.draw(rdr2);*/
+		}
+		window.draw(rectangle);
+		window.draw(circle);
+		ball.draw(window);
+		window.display();
+	}
+
+	return 0;
+}
